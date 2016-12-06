abbe5f762a731244260c02c945406978934f0050
luud <l.vankeulen@student.fontys.nl>
2016-12-06 09:57:09 +0100

    Init

---
+++ src/main/java/auction/domain/User.java
@@ -0,0 +1,34 @@
+package auction.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "User.count", query = "select count(u) from User u"),
+        @NamedQuery(name = "User.findByEmail", query = "select u from User u where u.email = :email")
+})
+public class User implements Serializable {
+
+    //@Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    //private Integer id;
+    @Id
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (this.email.equals(((User)o).getEmail()));
+    }
+}
